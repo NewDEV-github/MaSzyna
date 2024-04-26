1adbd691d044374fa0daaf178844d7b953f32955
DoS <karoltomaszewskimusic@gmail.com>
2024-04-26 17:41:43 +0200

    Fix missing unversioned files

---
+++ Assets/Scripts/Trains/Locos/Electric/203E.cs
@@ -0,0 +1,19 @@
+﻿namespace Trains.Locos.Electric
+{
+	public class _203E : TrainBase
+	{
+		public void Start()
+		{
+			TrainType = Type.ElectricLoco;
+			MaxSpeed = 125;
+			Acceleration = 2.5f;
+			Deceleration = 5;
+			Weight = 167f;
+			EngineState = Engine.StartElectric;
+			HourlyPower = 4160;
+			ContinuousPower = 4000;
+			TractionPower = 530;
+			HasCab = true;
+		}
+	}
+}
